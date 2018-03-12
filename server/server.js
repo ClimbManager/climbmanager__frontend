7911011319ec08108e9dc0ddc0f0a3173cf0e225
vvoronova <voronovaveronika@gmail.com>
2018-03-12 18:21:52 +0300

    Убрала консоль

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ const webpackDevHelper = require('./server.dev');
 
 const isDev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 8080;
-console.log('process.env.PORT ', process.env.CLIMBMAN_PORT );
 
 if (isDev) {
     webpackDevHelper.useWebpackMiddleware(app);
