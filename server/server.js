3011b5a3afca6019f0d042a0ccbc163e42baf25c
redveronika <voronovaveronika@gmail.com>
2018-03-12 17:59:39 +0300

    Заводимся на heroku

--- server/server.js
+++ server/server.js
@@ -5,7 +5,8 @@ const app = express();
 const webpackDevHelper = require('./server.dev');
 
 const isDev = process.env.NODE_ENV !== 'production';
-const port = process.env.CLIMBMAN_PORT || '5555';
+const port = process.env.CLIMBMAN_PORT || '9999';
+console.log('process.env.CLIMBMAN_PORT ', process.env.CLIMBMAN_PORT );
 
 if (isDev) {
     webpackDevHelper.useWebpackMiddleware(app);
